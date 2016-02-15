0b1985b3f812ea140e69c5cd8435d5acb047a97e
Matthew Wilcox <mwilcox.wi@gmail.com>
2016-02-15 15:13:59 -0600

    Commit
    More changes

--- app/src/main/java/com/cs407/thequiz/QuizActivity.java
+++ app/src/main/java/com/cs407/thequiz/QuizActivity.java
@@ -3,6 +3,8 @@ package com.cs407.thequiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.app.Fragment;
 
@@ -24,4 +26,28 @@ public class QuizActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
+
+
+
+
